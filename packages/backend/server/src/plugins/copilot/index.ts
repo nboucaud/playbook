6bf593bf57b9cb7364264e30ca1658053784a323
DarkSky <darksky2048@gmail.com>
2024-04-02 11:22:00 +0800

    fix: ci

--- packages/backend/server/src/plugins/copilot/index.ts
+++ packages/backend/server/src/plugins/copilot/index.ts
@@ -1,6 +1,5 @@
 import { ServerFeature } from '../../core/config';
 import { Plugin } from '../registry';
-import { CopilotController } from './controller';
 import { PromptService } from './prompt';
 import { assertProvidersConfigs, ProviderService } from './providers';
 import { ChatSessionService } from './session';
@@ -8,7 +7,6 @@ import { ChatSessionService } from './session';
 @Plugin({
   name: 'copilot',
   providers: [ChatSessionService, PromptService, ProviderService],
-  controllers: [CopilotController],
   contributesTo: ServerFeature.Copilot,
   if: config => {
     if (config.flavor.graphql) {
